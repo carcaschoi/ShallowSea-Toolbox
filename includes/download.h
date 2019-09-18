c9b40e11792b339d684e410cb88b77e138b05a32
Nathan <47043333+ITotalJustice@users.noreply.github.com>
2019-09-18 10:39:58 +0100

    Update download link for the app

--- includes/download.h
+++ includes/download.h
@@ -2,7 +2,7 @@
 #define _DOWNLOAD_H_
 
 #define SIGS_URL    "http://patches.totaljustice.xyz"
-#define APP_URL     "https://github.com/ITotalJustice/sigpatches-updater/releases/latest/download/sigpatches-updater.nro"
+#define APP_URL     "https://github.com/ITotalJustice/sigpatch-updater/releases/latest/download/sigpatch-updater.nro"
 
 int downloadFile(const char *url, const char *output);
 
