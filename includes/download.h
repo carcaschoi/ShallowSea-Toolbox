f89b4e46c6c3614e194566473d0d70ce7e4bc839
carcaschoi <64573431+carcaschoi@users.noreply.github.com>
2021-06-11 16:07:23 +0800

    Update download.h
    update ShallowSea-ams url

--- includes/download.h
+++ includes/download.h
@@ -1,10 +1,11 @@
 #ifndef _DOWNLOAD_H_
 #define _DOWNLOAD_H_
 
-#define AMS_SIG_URL     "https://github.com/ITotalJustice/patches/releases/latest/download/fusee.zip"
-#define HEKATE_SIG_URL  "https://github.com/ITotalJustice/patches/releases/latest/download/hekate.zip"
-#define APP_URL         "https://github.com/ITotalJustice/sigpatch-updater/releases/latest/download/sigpatch-updater.nro"
-#define TEMP_FILE       "/switch/sigpatch-updater/temp"
+//define ShallowSea-ams URL
+
+#define AMS_URL     "https://github.com/carcaschoi/ShallowSea/releases/latest/download/ShalowSea-ams.zip"
+#define APP_URL         "https://github.com/carcaschoi/ShallowSea_updater/releases/latest/download/ShallowSea-updater.nro"
+#define TEMP_FILE       "/switch/ShallowSea-updater/temp"
 
 #define ON              1
 #define OFF             0
